fc49e61ad55f3e8445c2ec2874998717ae26da6e
catancs <catalinniculescu20@gmail.com>
2024-04-26 15:54:29 +0200

    Home page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./Home";
-import About from "./About";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About'; 
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} exact />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </Router>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,42 +1,81 @@
 import React from 'react';
 import { Container, Navbar, Nav } from 'react-bootstrap';
-import './Home.css';
+import { Link } from 'react-router-dom';
+import './Home.css'; 
+
+
 
 
 function Home() {
   return (
-    <>
-      <Container className="text-center mt-4">
-      <h1 className="welcome-message">Hello, I'm Catalin</h1>
-
-      </Container>
-      <Navbar bg="dark" variant="dark" expand="lg">
+    <div className="beige-background">
+      <Navbar className="blue-nav shadow-sm mb-0" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">My Portfolio</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">My Portfolio</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/" className="nav-link">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about" className="nav-link">About</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      {/* Section*/}
-      <Container className="mt-3">
-      <section id="about" className="my-5">
-  <h2>About Me</h2>
-  {/* Content for About Me section will go here */}
-</section>
-<section id="projects" className="my-5">
-  <h2>Projects</h2>
-  {/* Content for Projects section will go here */}
-</section>
-<section id="contact" className="my-5">
-  <h2>Contact</h2>
-  {/* Content for Contact section will go here */}
-</section>
-
+      <Container className="text-center">
+        <h1 className="welcome-message">Welcome to my website!</h1>
       </Container>
-    </>
+      <Container className="paper-sheet">
+        <section id="about" className="my-5">
+          <h2>#About Me</h2>
+          <p>I'm a 3rd year Computer Science student at <a href="https://vu.nl/nl" target="_blank" rel="noopener noreferrer" className="about-link" >Vrije Universiteit Amsterdam</a>, 
+          currently doing my thesis in the <a href="https://research.vu.nl/en/organisations/software-and-sustainability-s2" className="about-link" target="_blank" rel="noopener noreferrer">Software and Sustainability (S2)</a> research group. 
+             I minored in <a href="https://studiegids.vu.nl/EN/Minor/2023-2024/data-science#/tab=2&code=&panel=" target="_blank" rel="noopener noreferrer" className="about-link" >Data Science</a> and I'm interested in Machine Learning and Artificial Intelligence.
+             I also work part-time as a student teaching assistant, helping younger students in the learning process.
+          </p>   
+          
+            </section>
+        <section id="projects" className="my-5">
+          <h2>#Professional Experience</h2>
+          <p>
+            <strong style={{ fontSize: '1.2em' }}>Warehouse Optimization Associate</strong><br />
+            <a href="https://www.intelligentfridges.com/" target="_blank" rel="noopener noreferrer" className="about-link" >Husky Intelligent Fridges</a> | October 2022 – February 2023<br />
+              - Specialized in software process optimization, hardware assembly, and database management.<br />
+              - Provided QA, B2B, and B2C remote/on-site technical support.<br />
+              - Configured Raspberry Pi devices and conducted FAT testing.
+          </p>
+            <p>
+            <strong style={{ fontSize: '1.2em' }}>Junior Network Engineer Intern</strong><br />
+            CONS IT International| July 2020 – September 2020<br />
+              - Assisted in designing and implementing network infrastructures, including routers, switches, and firewalls.<br />
+              - Gained experience with TCP/IP, VoIP, VPN, DNS, VLAN, and QoS technologies.
+            </p>
+        </section>
+        <section id="contact" className="my-5">
+          <h2>Skills</h2>
+          <p> 
+
+              - Python, TensorFlow, Keras, Streamlit<br />
+              - Node.js, RESTful APIs, SQLite<br />
+              - Machine Learning, AI, Data Analysis<br />
+              - Networking (TCP/IP, DNS, VPN) <br />
+              - Agile Methodologies, DevOps, CI/CD
+
+
+          </p>
+        </section>
+
+        <section id="contact" className="my-5">
+          <h2>Certifications</h2>
+          {/* Content for Contact section will go here */}
+        </section>
+
+        <section id="contact" className="my-5">
+          <h2>Links</h2>
+          {/* Content for Contact section will go here */}
+        </section>
+      </Container>
+    </div>
   );
 }
 
+
 export default Home;
+
+
